49b3b0962fd254cb410ed23e0e3add9957c63171
Robson de Moraes Silva <rms2307@outlook.com>
2024-07-14 21:32:25 -0300

    Inicia projeto

---
+++ Net.Delivery.Order.Domain/Services/NotifierService.cs
@@ -0,0 +1,29 @@
+﻿using Net.Delivery.Order.Domain.Entities;
+using System;
+
+namespace Net.Delivery.Order.Domain.Services
+{
+    /// <summary>
+    /// Notifier service
+    /// </summary>
+    public class NotifierService : INotifierService
+    {
+        /// <summary>
+        /// Notifies the customer about some order update
+        /// </summary>
+        /// <param name="order">Order data</param>
+        public void Notify(Entities.Order order)
+        {
+            SendEmail(order.Customer.Email);
+        }
+
+        /// <summary>
+        /// Sends email about order update to customer
+        /// </summary>
+        /// <param name="customer">Customer data</param>
+        private void SendEmail(string email)
+        {
+            Console.WriteLine("Email sent to recipient: " + email);
+        }
+    }
+}
